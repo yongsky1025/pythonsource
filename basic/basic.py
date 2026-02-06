3c6b68ddd13a6a920e4012150e952babdd55375f
yongsky1025 <qaz7984@gmail.com>
2026-02-06 18:00:38 +0900

    파이썬 타입, 함수, 텍스트CRUD

---
+++ basic/basic.py
@@ -0,0 +1,20 @@
+# 주석
+print("파이썬 프로그래밍")
+
+# msg = input("input num : ")
+# print(msg)
+
+str1 = "Life is too short, You need Python"
+str1 = "Life is too short, You need Python"
+str1 = """Life is too short, You need Python"""
+str1 = """Life is too short, You need Python"""
+
+print(str1)
+
+
+str1 = """\
+Life is too short, 
+You need Python
+"""
+
+print(str1)
